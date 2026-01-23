ed773d10b80b55c0e93311a34890689bd6917cf8
Manish Kumar <mkjangra06@gmail.com>
2026-01-23 10:32:42 +0530

    Handle File Stuffs | User Decide File Content

--- ssh-configuration.sh
+++ ssh-configuration.sh
@@ -14,16 +14,11 @@ Workflow
 
 SSH_FILE_DIR=~/.ssh
 PUBLIC_FILE=${SSH_FILE_DIR}/${FILE_NAME}.pub
-FOLDER_DIR=~/Desktop/Folder1
+FOLDER_DIR=~/Desktop/ServerConfig
 
 echo "===================================="
 echo "SSH KEY GENERATING STEPS"
 echo "===================================="
-
-echo "Make SSH Folder in Local Machine"
-sudo mkdir -p ${SSH_FILE_DIR}
-
-echo "Folder Created Done"
 
 echo "Making A SSH Key Using ssh-keygen"
 read -p "Enter SSH File Name: " FILE_NAME
@@ -38,16 +33,38 @@ echo ""
 read -p "Enter SERVER User Name: " SERVER_USER
 read -p "Enter SERVER IP (192.168.x.x): " SERVER_IP
 echo "Copying SSH KEY to Server..."
-ssh-copy-id -o StrictHostKeyChecking=no -i ${SSH_FILE_DIR}/${FILE_NAME}.pub ${SERVER_USER}@${SERVER_IP}
+ssh-copy-id -i ${SSH_FILE_DIR}/${FILE_NAME}.pub ${SERVER_USER}@${SERVER_IP}
 echo "SSH Key Successfully Transfered to Host Address"
 echo ""
 echo "===================================="
 echo "MAKE A FOLDER AND FILE"
 echo "===================================="
 # read -p "Enter Folder Name: " FOLDER_NAME
-mkdir -p ~/Desktop/Folder1
+mkdir -p ~/Desktop/ServerConfig
 echo "Setup.txt File Generated In Folder"
-echo -e "sudo apt update \nsudo apt install nginx -y" >> ${FOLDER_DIR}/setup.sh
+if [[ -f ${FOLDER_DIR}/ServerSetup.sh ]]; 
+then
+	echo "File Already Exist in ${FOLDER_DIR}"
+	read -p "If you want Edit Existed file (y/n): " CHOOSE_OPTION
+	if [[ ${CHOOSE_OPTION} == "y" || ${CHOOSE_OPTION} == "Y" ]];
+	then
+		read -p "Your want to Overwrite in file (y/n): " OVERWRITE
+		if [[ ${OVERWRITE} == "y" || ${OVERWRITE} == "Y" ]]; 
+		then
+			echo "Enter your commands. Press Enter then Ctrl+D to finish."
+			cat > "${FOLDER_DIR}/ServerSetup.sh"
+		else
+			echo "Adding Commands in Existing File"
+			echo "Enter commands. Press Enter then Ctrl+D to finish"
+			cat >> "${FOLDER_DIR}/ServerSetup.sh"
+		fi
+	fi
+else
+	echo "Which Commands want to run in Server?"
+	echo "--- Type your commands. Press Enter then Ctrl+D to finish ---"
+	cat > "${FOLDER_DIR}/ServerSetup.sh"
+	echo -e "\nServerSetup.txt File Generated In Folder"
+fi
 echo "FILE GENERATED AND STORE IN FOLDER"
 echo ""
 echo "====================================="
@@ -58,4 +75,5 @@ scp -r ${FOLDER_DIR} ${SERVER_USER}@${SERVER_IP}:/home/${SERVER_USER}
 echo ""
 echo "FILE & FOLDER TRANSFER DONE"
 echo "GIVE PERMISSION TO FILE FOR EXECUTE"
-ssh -t ${SERVER_USER}@${SERVER_IP} "chmod +x ~/Folder1/setup.sh && bash ~/Folder1/setup.sh"
+ssh -t ${SERVER_USER}@${SERVER_IP} "chmod +x ~/ServerConfig/ServerSetup.sh && bash ~/ServerConfig/ServerSetup.sh"
+echo "\nServer Setup is Done !"
