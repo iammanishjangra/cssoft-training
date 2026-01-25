03f003d54e0861e36f38618adc59e6748800de2f
Manish Kumar <mkjangra06@gmail.com>
2026-01-25 20:21:41 +0530

    Linux Assignment

--- linux-assignment/setup.sh
+++ linux-assignment/setup.sh
@@ -15,7 +15,7 @@ SCREENSHOT=${WORK_DIR}/screenshots
 task_1() {
 clear
 echo "========================================================================="
-echo "                      NAVIGATION & DIRECTORIES"
+echo "                    TASK 1: NAVIGATION & DIRECTORIES"
 echo "========================================================================="
 echo ""
 echo "[+] CHECK CURRENT DIRECTORY [+]"
@@ -165,6 +165,50 @@ echo ""
 
 # Preview Screenshot
 feh ${SCREENSHOT}/Task2_FileOperations.png
+
+# Generating TXT File
+echo "[+] GENERATING FILE TASK 2 [+]"
+echo "
+=========================================================================
+                    TASK 2:  FILE OPERATIONS
+=========================================================================
+
+[+] CREATE LOGS FOLDER [+]
+
+> COMMAND:- mkdir ~/logs
+> COMMAND OVERVIEW:- Creating Folder in Home Directory using mkdir Command.
+> OUTPUT:- No Output Appear While Execute this command. Its run on background.
+
+[+] MOVE file1.txt INTO Logs FOLDER [+]
+
+> COMMAND:- mv ~/devops_practice/file1.txt ~/logs
+> COMMAND OVERVIEW:- mv Command stands for Moving any File and Directories. Here we Move file.txt File from devops_practice Directory to Logs Directory.
+> OUTPUT:- No Output Appear While Execute this Command. Its run on Background.
+
+[+] RENAME file2.txt to config.txt [+]
+
+> COMMAND:- mv ~/devops_practice/file2.txt ~/devops_practice/config.txt
+> COMMAND OVERVIEW:- We also Used mv Command for Rename any Files and Directories.
+> OUTPUT:- No Output Appear While Execute this Command. Its run on Background.
+
+[+] COPY file3.txt TO backup.txt FILE [+]
+
+> COMMAND:- cp ~/devops_practice/file3.txt ~/devops_practice/backup.txt
+> COMMAND OVERVIEW:- cp stands for COPY any File and Directories. cp Commands make a Same Carbon copy of File or Directories.
+> OUTPUT:- No Output Appear While Execute this command. Its run on Background.
+
+[+] DELETE LOGS FOLDER [+]
+
+> COMMAND:- rm -rv ~/logs
+> COMMAND OVERVIEW:- rm stands for Remove any File and Directory. -rv Flags work are different. When Folder is empty, then we can't use any addtional flags but when folder is not empty then we must use -r (Recursive mode) flag for delete Directory + Files. -v (verbose) show live action on terminal which file and folder deleted.
+> OUTPUT:-
+	removed 'logs/file2.txt'
+	removed directory 'logs/'
+
+" >> ${WORK_DIR}/linux-assignment.txt
+echo ""
+echo "File Generated !"
+sleep 2
 clear
 }
 
@@ -182,7 +226,7 @@ echo "Writing...."
 echo -e "Hello Connections!
 My name is Manish Kumar from Hisar, Haryana.
 I have completed my Higher Secondary Schooling.
-I am learning DevOps Engineering in CS Soft Solutions (Mohali)" > ~/devops_practice/config.txt
+I am learning DevOps Engineering in CS Soft Solutions, Mohali" > ~/devops_practice/config.txt
 echo ""
 echo -e "${GREEN}config.txt${NC} file Modified."
 echo ""
@@ -213,6 +257,46 @@ echo ""
 
 # Preview Screenshot
 feh ${SCREENSHOT}/Task3_FileContent.png
+
+# Generating TXT File
+echo "[+] GENERATING FILE TASK 3 [+]"
+echo "
+=========================================================================
+                      TASK 3: FILE CONTENT
+=========================================================================
+
+[+] WRITE SAMPLE TEXT IN config.txt FILE [+]
+
+> COMMAND:- echo 'Hello Connections!
+My name is Manish Kumar from Hisar, Haryana.
+I have completed my Higher Secondary Schooling.
+I am learning DevOps Engineering in CS Soft Solutions, Mohali' > > ~/devops_practice/config.txt
+> COMMAND OVERVIEW:- echo command use for print any text but here we use for append in config.txt file. > > used for append the test in target file.
+> OUTPUT:- Run on Background.
+
+[+] DISPLAY CONTENT [+]
+
+> COMMAND:- cat ~/devops_practice/config.txt
+> COMMAND OVERVIEW:- cat stands for print files content in terminal.
+> OUTPUT:-
+	Hello Connections!
+	My name is Manish Kumar from Hisar, Haryana.
+	I have completed my Higher Secondary Schooling.
+	I am learning DevOps Engineering in CS Soft Solutions, Mohali
+
+[+] SHOW FIRST AND LAST LINE FROM config.txt FILE [+]
+
+> COMMAND:- 'head -n 1 ~/devops_practice/config.txt' 'For Print Top Lines from File'
+	    'tail -n 1 ~/devops_practice/config.txt' 'For Print Bottom Lines from File'
+> COMMAND OVERVIEW:- head stands for print top lines from any files and tail stands for print bottom lines from any files.
+> OUTPUT:-
+	When we run Head Command then result is:- Hello Connections!
+	When we run tail Command then result is:- I am learning DevOps Engineering in CS Soft Solutions, Mohali
+
+" >> ${WORK_DIR}/linux-assignment.txt
+echo ""
+echo "File Generated !"
+sleep 2
 clear
 }
 
@@ -253,6 +337,30 @@ echo ""
 
 # Preview Screenshot
 feh ${SCREENSHOT}/Task4_Permissions.png
+# Generating TXT File
+echo "[+] GENERATING FILE TASK 4 [+]"
+echo "
+
+=========================================================================
+                        TASK 4: PERMISSIONS
+=========================================================================
+
+[+] CHECK PERMISSIONS OF config.txt [+]
+
+> COMMAND:- ls -l ~/devops_practice/config.txt
+> COMMAND OVERVIEW:- ls use for List and -l flags Use for Print more information about config.txt File, like permissions, pid, gid, size, name etc.
+> OUTPUT:- -rw-rw-r-- 1 cssoft cssoft 174 Jan 25 18:01 /home/cssoft/devops_practice/config.txt
+
+[+] ALLOW ONLY READ AND WRITE TO OWNER
+
+> COMMAND:- chmod 600 ~/devops_practice/config.txt
+> COMMAND OVERVIEW:- chmod stands for Change Mode. Using this commands we can change the Read | Write | Execute Permission of All Type of Files and Directories.
+> OUTPUT:- Run on Background but when we check this its show '-rw-------' permission.
+
+" >> ${WORK_DIR}/linux-assignment.txt
+echo ""
+echo "File Generated !"
+sleep 2
 clear
 }
 
@@ -268,6 +376,10 @@ echo -e "[+] SEARCH ${BLUE}DevOps${NC} WORD IN ${RED}config.txt${NC} [+]"
 echo ""
 cd ~/devops_practice
 echo -e "Searched Result: ${GREEN}$(grep -o DevOps ./config.txt)${NC}"
+echo ""
+echo -e "[+] FIND ${RED}config.txt${NC} FILE"
+echo ""
+echo -e "File Founded: ${GREEN}$(find ~/devops_practice -name "config.txt")${NC}"
 echo ""
 echo "========================================================================="
 echo "                 TASK 5 IS COMPLETED ( wait 3 seconds )"
@@ -287,6 +399,29 @@ echo ""
 
 # Preview Screenshot
 feh ${SCREENSHOT}/Task5_SearchFind.png
+# Generating TXT File
+echo "[+] GENERATING FILE TASK 5 [+]"
+echo "
+=========================================================================
+                          TASK 5: SEARCH & FIND
+=========================================================================
+
+[+] SEARCH 'DevOps' WORD IN config.txt
+
+> COMMAND:- grep -o DevOps ./config.txt
+> COMMAND OVERVIEW:- grep used for Search Text Content from File content. -o flag use for Only this text print in terminal.
+> OUTPUT:- DevOps
+
+[+] FIND config.txt FILE IN SYSTEM
+
+> COMMAND:- find ~/devops_practice -name 'config.txt'
+> COMMAND OVERVIEW:- find command use for search any file and directory from system, here we decide the path for searching criteria.
+> OUTPUT:- /home/cssoft/devops_practice/config.txt
+
+" >> ${WORK_DIR}/linux-assignment.txt
+echo ""
+echo "File Generated !"
+sleep 2
 clear
 }
 
@@ -336,6 +471,67 @@ echo ""
 
 # Preview Screenshot
 feh ${SCREENSHOT}/Task6_SystemInformation.png
+
+# Generating TXT File
+echo "[+] GENERATING FILE TASK 6 [+]"
+echo "
+=========================================================================
+                      TASK 6: SYSTEM INFORMATION
+=========================================================================
+
+[+] CHECK USERNAME
+
+> COMMAND:- whoami
+> COMMAND OVEVIEW:- whoami used for print Username in Terminal
+> OUTPUT:- cssoft
+
+[+] CHECK HOSTNAME [+]
+
+> COMMAND:- hostname
+> COMMAND OVERVIEW:- hostname used for print Hostname in Terminal
+> OUTPUT:- ubuntu
+
+[+] RUNNING TOP 5 PROCESS
+
+> COMMAND:- ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%cpu | head -5
+> COMMAND OVERVIEW:- ps stands for process status. This command use for check running process in our machine.
+> OUTPUT:-
+	    PID    PPID CMD                         %MEM %CPU
+	  14643    9965 ps -eo pid,ppid,cmd,%mem,%c  0.0  700
+	  14469    4451 gjs /usr/share/gnome-shell/  0.8  3.6
+	   4451    4131 /usr/bin/gnome-shell         4.3  2.8
+	   9682    4131 /usr/libexec/gnome-terminal  0.8  0.9
+
+[+] DISK USAGES
+
+> COMMAND:- df -h
+> COMMAND:- df stands for Disk Free Status and also give all information about Disk Usages.
+> OUTPUT:-
+	Filesystem      Size  Used Avail Use% Mounted on
+	tmpfs           779M  2.6M  777M   1% /run
+	/dev/sdb3       223G   78G  133G  37% /
+	tmpfs           3.9G   45M  3.8G   2% /dev/shm
+	tmpfs           5.0M  8.0K  5.0M   1% /run/lock
+	efivarfs        118K   75K   39K  66% /sys/firmware/efi/efivars
+	tmpfs           3.9G     0  3.9G   0% /run/qemu
+	/dev/sdb1       489M  6.2M  482M   2% /boot/efi
+	tmpfs           779M  116K  779M   1% /run/user/1000
+	tmpfs           779M  164K  779M   1% /run/user/1001
+
+[+] MEMORY USAGES
+
+> COMMAND:- free -h
+> COMMAND OVERVIEW:- free command stands for free memory status.
+> OUTPUT:-
+	               total        used        free      shared  buff/cache   available
+	Mem:           7.6Gi       3.9Gi       2.0Gi       943Mi       3.0Gi       3.7Gi
+	Swap:           11Gi          0B        11Gi
+
+" >> ${WORK_DIR}/linux-assignment.txt
+echo ""
+echo "File Generated !"
+sleep 2
+clear
 }
 
 # Task 7: Archive & Compress
@@ -381,6 +577,41 @@ echo ""
 
 # Preview Screenshot
 feh ${SCREENSHOT}/Task7_ArchiveExtract.png
+# Generating TXT File
+echo "[+] GENERATING FILE TASK 7 [+]"
+echo "
+
+=========================================================================
+                      TASK 7: ARCHIVE & COMPRESS
+=========================================================================
+
+[+] CREATE TAR ARCHIVE OF devops_practice FOLDER [+]
+
+> COMMAND:- tar -cvf backup.tar ./devops_practice
+> COMMAND OVERVIEW:- tar command use for make a Compress Archive of Any Single file and group of file and directories.
+		-c for Create
+		-v for verbose
+		-f for file
+> OUTPUT:-
+	./devops_practice/
+	./devops_practice/backup.txt
+	./devops_practice/backup.tar
+	./devops_practice/file3.txt
+	./devops_practice/config.txt
+
+[+] EXTRACT ARCHIVE TAR FILE [+]
+
+> COMMAND:- tar -xvf backup.tar -C ~/Desktop
+> COMMAND OVERVIEW:- Using of tar command we also extract any compress files. but we use different flags
+		-x for Extract
+		-v for verbose
+		-f for file
+		-C for File Output Destination
+
+" >> ${WORK_DIR}/linux-assignment.txt
+echo ""
+echo "File Generated !"
+sleep 2
 clear
 }
 
@@ -401,7 +632,7 @@ echo -e "${GREEN}$(ping -c 5 www.google.com)${NC}"
 echo ""
 echo "[+] CHECK DNS RESOLUTION"
 echo ""
-echo -e "${GREEN}$(nslookup www.google.com)${NC}"
+echo -e "${GREEN}$(nslookup google.com)${NC}"
 echo ""
 << PublicIP
 echo "{+] PUBLIC IP ADDRESS [+]"
@@ -430,14 +661,84 @@ echo ""
 # Preview Screenshot
 feh ${SCREENSHOT}/Task8_Networking.png
 
+# Generating TXT File
+echo "[+] GENERATING FILE TASK 8 [+]"
+echo "
+
+=========================================================================
+                         TASK 8: NETWORKING
+=========================================================================
+
+[+] FIND IP ADDRESS [+]
+
+> COMMAND:- hostname -I | awk '{print $1}'
+> COMMAND OVERVIEW:- We check hostname using hostname command but When we Check IP Address, then we just add -I flag.
+> OUTPUT:- 192.168.31.226
+
+[+] PING google.com '5 PACKETS' [+]
+
+> COMMAND:- ping -c 5 google.com
+> COMMAND OVERVIEW:- Using of ping command, We check Our Website aur Server is Online/Offline. When we get Packets line by line then Assume that Our Server and Website is Live.
+> OUTPUT:-
+	PING google.com (2404:6800:4009:804::200e) 56 data bytes
+	64 bytes from bom05s05-in-x0e.1e100.net (2404:6800:4009:804::200e): icmp_seq=1 ttl=116 time=43.4 ms
+	64 bytes from bom05s05-in-x0e.1e100.net (2404:6800:4009:804::200e): icmp_seq=2 ttl=116 time=133 ms
+	64 bytes from bom05s05-in-x0e.1e100.net (2404:6800:4009:804::200e): icmp_seq=3 ttl=116 time=43.2 ms
+	64 bytes from bom05s05-in-x0e.1e100.net (2404:6800:4009:804::200e): icmp_seq=4 ttl=116 time=42.9 ms
+	64 bytes from bom05s05-in-x0e.1e100.net (2404:6800:4009:804::200e): icmp_seq=5 ttl=116 time=107 ms
+
+	--- google.com ping statistics ---
+	5 packets transmitted, 5 received, 0% packet loss, time 4004ms
+	rtt min/avg/max/mdev = 42.897/73.970/133.073/38.612 ms
+
+[+] CHECK DNS RESOLUTION [+]
+
+> COMMAND:- nslookup google.com
+> COMMAND OVERVIEW:- nslookup command Find all DNS Records for a domain name.
+> OUTPUT:-
+	Server:		127.0.0.53
+	Address:	127.0.0.53#53
+
+	Non-authoritative answer:
+	Name:	google.com
+	Address: 142.251.43.174
+	Name:	google.com
+	Address: 2404:6800:4009:801::200e
+
+" >> ${WORK_DIR}/linux-assignment.txt
+echo ""
+echo "File Generated !"
+sleep 2
+
 read -p "Want to Remove All Unnecessary Files (y/n)" CHOOSE
 if [[ ${CHOOSE} == 'y' || ${CHOOSE} == 'Y' ]];
 then
 	echo -e "Removing ${GREEN}devops_practice${NC}, ${GREEN}Screenshots${NC}, ${GREEN}Archive Files${NC}"
-	rm -rf ~/devops_practice ~/backup.tar ~/Desktop/devops_practice ${WORK_DIR}/screenshots
+	rm -rf ~/devops_practice ~/backup.tar ~/Desktop/devops_practice ${WORK_DIR}/screenshots ${WORK_DIR}/linux-assignment.txt
 	echo "All Files Removed "
 else
 	echo "No Issue ! Do Manually :D"
+fi
+
+read -p "Want to Upload All Created Files and Directories to Server [y/n]: " CHOOSESERVER
+if [[ ${CHOOSESERVER} == 'y' || ${CHOOSESERVER} == 'Y' ]];
+then
+	echo "Starting to Upload AT Server..."
+	echo -e "${GREEN}We Use Existed Keypair for Connecting with Server${NC}"
+	read -p "Enter Your Server User Name (Default: manish) : " USERNAME
+	USERNAME=${USERNAME:-manish}
+	read -p "Enter Your Server IP (192.168.x.x): " SERVER_IP
+	SERVER_IP=${SERVER_IP:-192.168.0.1}
+	echo "Copying SSH Keypair In Server"
+	ssh-copy-id -i ~/.ssh/manish.pub ${USERNAME}@${SERVER_IP}
+	echo "SSH Key Successfully Transfered to Host Address."
+	echo ""
+	echo "Copy All Files In Server..."
+	scp -r ~/Desktop/cssoft-training/linux-assignment ${USERNAME}@${SERVER_IP}:/home/${USERNAME}
+	echo "File Transfered !"
+	echo ""s
+else
+	echo "Do Manually..."
 fi
 sleep 2
 clear
